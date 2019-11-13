7c9cc10246c77f60912a7e3e3edb5fd0e2883a70
gpogoncheff <galenpogoncheff@me.com>
2019-11-13 15:44:18 -0700

    Added logginf of rest-server requests

---
+++ logging/logs.py
@@ -0,0 +1,22 @@
+import pika
+import sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='logs', exchange_type='topic')
+
+result = channel.queue_declare('', exclusive=True)
+queue_name = result.method.queue
+
+# listen to any log message
+channel.queue_bind(exchange='logs', queue=queue_name, routing_key='#')
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+def callback(ch, method, properties, body):
+    print(" [x] %r: %r" % (method.routing_key, body))
+    print()
+
+channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+channel.start_consuming()
--- rest-server/server.py
+++ rest-server/server.py
@@ -8,6 +8,7 @@ import pika
 import pickle
 import socket
 import sys
+import socket
 from utils.Datastore import get_aggregate_statistics, get_ride_data
 from utils.CloudStorage import get_visualization_url
 
@@ -30,15 +31,10 @@ def publish_work_request(data, host='rabbitmq', queue_name='work_route'):
         )
         success = True
 
-    except pika.exceptions.AMQPConnectionError:
+    except (pika.exceptions.AMQPConnectionError, pika.exceptions.AMPQChannelError):
         # Debug
-        print("Connection error")
-
-    except pika.exceptions.AMPQChannelError:
-        # Debug
-        print("Channel Error")
-        if connection is not None:
-            connection.close()
+        print("AMPQ Error")
+        success = False
 
     if connection is not None:
         connection.close()
@@ -46,11 +42,30 @@ def publish_work_request(data, host='rabbitmq', queue_name='work_route'):
     return success
 
 
-def log_request(request, status):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
-    log_channel = connection.channel()
-    log_channel.exchange_declare(exchange='logs', exchange_type='topic')
-    connection.close()
+def log_api_status(api, success, message='', host='rabbitmq'):
+    # log the api that was called and the result of the api call
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+        log_channel = connection.channel()
+        log_channel.exchange_declare(exchange='logs', exchange_type='topic')
+
+        log_channel.basic_publish(
+            exchange='logs',
+            routing_key='{}.rest.info'.format(socket.gethostname()),
+            body='API Call: {} - {}'.format(api, message)
+        )
+        if not success:
+            log_channel.basic_publish(
+                exchange='logs',
+                routing_key='{}.rest.debug'.format(socket.gethostname()),
+                body='API Call: {} - FAILED; {}'.format(api, message)
+            )
+    except:
+        print('Failed to log api info')
+
+    if connection is not None:
+        connection.close()
+
 
 # Test route
 @app.route('/', methods=['GET', 'POST'])
@@ -64,13 +79,23 @@ def process_data(filename):
     try:
         hash = str(hashlib.md5(r.data).hexdigest())
         data = pickle.dumps((hash, filename, r.data))
-        publish_work_request(data, host='localhost')
+        work_published = publish_work_request(data, host='localhost')
         response = {'md5 hash' : hash}
-        status=200
+        status = 200 if work_published else 500
     except BaseException as e:
         print('Error: upload-ride - {}'.format(e))
         response = { 'md5 hash' : 'None'}
-        status=400
+        status = 400
+
+    success = False
+    if status == 200:
+        message = 'SUCCESS'
+        success = True
+    elif status == 500:
+        message = 'Failed to publish to RabbitMQ'
+    else:
+        message = 'FAILED'
+    log_api_status('/upload-ride/{}'.format(filename), success, message, host='localhost')
 
     return Response(response=jsonpickle.encode(response), status=status, mimetype="application/json")
 
@@ -85,6 +110,14 @@ def get_global_statistics():
         data = {}
         status = 400
 
+    success = False
+    if status == 200:
+        message = 'SUCCESS'
+        success = True
+    else:
+        message = 'FAILED'
+    log_api_status('/global-stats', success, message, host='localhost')
+
     return Response(response=jsonpickle.encode(data), status=status, mimetype="application/json")
 
 
@@ -98,17 +131,38 @@ def get_data_for_ride(digest):
         data = []
         status = 400
 
+    success = False
+    if status == 200:
+        message = 'SUCCESS'
+        success = True
+    else:
+        message = 'FAILED'
+    log_api_status('/ride-data/{}'.format(digest), success, message, host='localhost')
+
     response = {'segments': data}
     return Response(response=jsonpickle.encode(response), status=status, mimetype="application/json")
 
 
 @app.route('/visualize/<digest>/<segment>', methods=['GET'])
 def get_ride_visualizations(digest, segment):
-    url = get_visualization_url(digest, segment)
+    try:
+        url = get_visualization_url(digest, segment)
+        status = 200
+    except BaseException as e:
+        print('Error: visualize - {}'.format(e))
+        status = 400
+
+    success = False
+    if status == 200:
+        message = 'SUCCESS'
+        success = True
+    else:
+        message = 'FAILED'
+    log_api_status('/visualize/{}/{}'.format(digest, segment), success, message, host='localhost')
+
     return redirect(url, code=302)
 
 
 if __name__ == '__main__':
     # start flask app
     app.run(host="0.0.0.0", port=5000)
-    connection.close()
